da5d11c549aa9c832a9b72f25b76b6164a938ff0
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-11-01 16:04:37 -0600

    Se realizan ejercicios

--- Clase1/variables.js
+++ Clase1/variables.js
@@ -1,2 +1,14 @@
 var nombre = 'Israel', apellido = ' Santizo';
 console.log('Hola mi nombre es: '+nombre + apellido )
+var nombreMayuscula = nombre.toUpperCase()
+var apellidoMinuscula = apellido.toLowerCase()
+var primeraLetraNombre = nombre.charAt(0)
+var cantidadLetraNombre = nombre.length
+var nombreCompleto = nombre + ' ' + apellido
+/*Interpolacion*/
+var nombreCompletoInterpolacion = `${nombre} ${apellido.toUpperCase()}`
+/*Sub string*/
+var str = nombre.substr(1, 2)
+/*Ejercicio*/
+var ultimaLetraNombreSub = nombre.substr(-1)
+var ultimaLetraNombreChart = nombre.charAt(nombre.length-1)
